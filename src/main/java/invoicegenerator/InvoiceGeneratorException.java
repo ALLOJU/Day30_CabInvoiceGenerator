ceee7ac5f0a61501233cef873c2a4b81d12db8ad
ALLOJU <mounika.alloju@gmail.com>
2022-03-25 15:07:38 +0530

    given userid returns all invoice summary

---
+++ src/main/java/invoicegenerator/InvoiceGeneratorException.java
@@ -0,0 +1,17 @@
+package invoicegenerator;
+
+public class InvoiceGeneratorException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public enum ExceptionType {
+		USER_ALREADY_EXISTS
+	}
+
+	public ExceptionType type;
+
+	public InvoiceGeneratorException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+
+}
--- src/main/java/invoicegenerator/Ride.java
+++ src/main/java/invoicegenerator/Ride.java
@@ -4,19 +4,28 @@ public class Ride {
 	
 	private double distance;
 	private final int time;
+	public String rideType;
 	public double getDistance() {
 		return distance;
 	}
 	public void setDistance(double distance) {
 		this.distance = distance;
 	}
+	public String getRideType() {
+		return rideType;
+	}
+	public void setRideType(String rideType) {
+		this.rideType = rideType;
+	}
 	public int getTime() {
 		return time;
 	}
-	public Ride(double distance, int time) {
+	public Ride(double distance, int time, String rideType) {
 		super();
 		this.distance = distance;
 		this.time = time;
+		this.rideType = rideType;
 	}
+	
 	
 }
